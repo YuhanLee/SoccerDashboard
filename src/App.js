c34813dfc55d5069caa54ce3b21787865fd3e243
Yuhan Lee <ylee050@uottawa.ca>
2019-03-13 12:24:09 -0400

    added simple resizable grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
+import './App.css';
+import 'react-grid-layout/css/styles.css' 
+import 'react-resizable/css/styles.css' 
+import MyFirstGrid from './components/MyFirstGrid';
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const App = () => (
+      <div>
+        <Switch>
+          <Route exact path='/' component={MyFirstGrid}/>
+        </Switch>
       </div>
+    )
+    return (
+      <Switch>
+        <App/>
+      </Switch>
     );
   }
 }
